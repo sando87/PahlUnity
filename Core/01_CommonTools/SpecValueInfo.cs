208bf13bbdd7bca42198285004a09688b8cf21f5
sjlee <tjdwn0406@gmail.com>
2026-06-06 02:04:48 +0900

    SpecValueInfo 스펙필드 인스펙터 표기 개선

--- Core/01_CommonTools/SpecValueInfo.cs
+++ Core/01_CommonTools/SpecValueInfo.cs
@@ -1,6 +1,10 @@
 using System;
-using NaughtyAttributes;
+using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace PahlUnity
 {
@@ -19,17 +23,20 @@ namespace PahlUnity
         [SerializeField] private SpecValueType _ValueType;
 
         bool IsNoneValueType => _ValueType == SpecValueType.Simple;
-        [SerializeField, AllowNesting, ShowIf(nameof(IsNoneValueType))] private float _Value;
+        [SerializeField] private float _Value;
 
         bool IsMinMaxValueType => _ValueType == SpecValueType.MinMax;
-        [SerializeField, AllowNesting, ShowIf(nameof(IsMinMaxValueType))] private float _Min;
-        [SerializeField, AllowNesting, ShowIf(nameof(IsMinMaxValueType))] private float _Max;
+        [SerializeField] private float _Min;
+        [SerializeField] private float _Max;
 
         bool IsCurveValueType => _ValueType == SpecValueType.Curve;
-        [SerializeField, AllowNesting, ShowIf(nameof(IsCurveValueType))] private AnimationCurve _Curve;
+        [SerializeField] private AnimationCurve _Curve;
 
         bool IsCustomValueType => _ValueType == SpecValueType.Custom;
-        [SerializeField, AllowNesting, ShowIf(nameof(IsCustomValueType))] private string _CustomValue;
+        [SerializeField] private string _CustomValue;
+
+        bool IsListValueType => _ValueType == SpecValueType.List;
+        [SerializeField] private List<float> _ListValues;
 
         [SerializeField] private float _Step;
 
@@ -95,5 +102,119 @@ namespace PahlUnity
         MinMax,
         Curve,
         Custom,
+        List,
     }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(SpecValueInfo))]
+    public class SpecValueInfoDrawer : PropertyDrawer
+    {
+        private const float VerticalSpacing = 2f;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            Rect currentRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            property.isExpanded = EditorGUI.Foldout(currentRect, property.isExpanded, label, true);
+
+            if (property.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+
+                DrawProperty(ref currentRect, property.FindPropertyRelative("_Key"));
+                DrawProperty(ref currentRect, property.FindPropertyRelative("_ModifierType"));
+
+                SerializedProperty valueTypeProperty = property.FindPropertyRelative("_ValueType");
+                DrawProperty(ref currentRect, valueTypeProperty);
+
+                SpecValueType valueType = (SpecValueType)valueTypeProperty.enumValueIndex;
+                DrawValueProperty(ref currentRect, property, valueType);
+
+                DrawProperty(ref currentRect, property.FindPropertyRelative("_Step"));
+
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float height = EditorGUIUtility.singleLineHeight;
+            if (!property.isExpanded)
+            {
+                return height;
+            }
+
+            height += GetPropertyHeight(property.FindPropertyRelative("_Key"));
+            height += GetPropertyHeight(property.FindPropertyRelative("_ModifierType"));
+
+            SerializedProperty valueTypeProperty = property.FindPropertyRelative("_ValueType");
+            height += GetPropertyHeight(valueTypeProperty);
+
+            SpecValueType valueType = (SpecValueType)valueTypeProperty.enumValueIndex;
+            height += GetValuePropertyHeight(property, valueType);
+            height += GetPropertyHeight(property.FindPropertyRelative("_Step"));
+
+            return height;
+        }
+
+        private void DrawValueProperty(ref Rect currentRect, SerializedProperty property, SpecValueType valueType)
+        {
+            switch (valueType)
+            {
+                case SpecValueType.Simple:
+                    DrawProperty(ref currentRect, property.FindPropertyRelative("_Value"));
+                    break;
+                case SpecValueType.MinMax:
+                    DrawProperty(ref currentRect, property.FindPropertyRelative("_Min"));
+                    DrawProperty(ref currentRect, property.FindPropertyRelative("_Max"));
+                    break;
+                case SpecValueType.Curve:
+                    DrawProperty(ref currentRect, property.FindPropertyRelative("_Curve"));
+                    break;
+                case SpecValueType.Custom:
+                    DrawProperty(ref currentRect, property.FindPropertyRelative("_CustomValue"));
+                    break;
+                case SpecValueType.List:
+                    DrawProperty(ref currentRect, property.FindPropertyRelative("_ListValues"));
+                    break;
+            }
+        }
+
+        private float GetValuePropertyHeight(SerializedProperty property, SpecValueType valueType)
+        {
+            switch (valueType)
+            {
+                case SpecValueType.Simple:
+                    return GetPropertyHeight(property.FindPropertyRelative("_Value"));
+                case SpecValueType.MinMax:
+                    return GetPropertyHeight(property.FindPropertyRelative("_Min")) +
+                        GetPropertyHeight(property.FindPropertyRelative("_Max"));
+                case SpecValueType.Curve:
+                    return GetPropertyHeight(property.FindPropertyRelative("_Curve"));
+                case SpecValueType.Custom:
+                    return GetPropertyHeight(property.FindPropertyRelative("_CustomValue"));
+                case SpecValueType.List:
+                    return GetPropertyHeight(property.FindPropertyRelative("_ListValues"));
+                default:
+                    return 0f;
+            }
+        }
+
+        private void DrawProperty(ref Rect currentRect, SerializedProperty property)
+        {
+            float height = EditorGUI.GetPropertyHeight(property, true);
+            currentRect.y += currentRect.height + VerticalSpacing;
+            currentRect.height = height;
+            EditorGUI.PropertyField(currentRect, property, true);
+        }
+
+        private float GetPropertyHeight(SerializedProperty property)
+        {
+            return EditorGUI.GetPropertyHeight(property, true) + VerticalSpacing;
+        }
+    }
+#endif
 }
