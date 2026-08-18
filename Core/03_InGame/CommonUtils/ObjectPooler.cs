5fbd281ab9f159d92b0c88b8abc8b6c3bf639819
sjlee <tjdwn0406@gmail.com>
2026-08-18 21:40:08 +0900

    객체 VFX 생성 기능 개선

--- Core/03_InGame/CommonUtils/ObjectPooler.cs
+++ Core/03_InGame/CommonUtils/ObjectPooler.cs
@@ -13,5 +13,13 @@ namespace PahlUnity
             else
                 ObjectPoolingManager.Instance.PopForDuration(poolingObj, poolingObj.transform.position, poolingObj.transform.rotation, null, 3);
         }
+
+        public void InstantiateVFX(Collider target)
+        {
+            Vector3 pos = target.ExGetBase().Body3D.Center;
+            pos.y = 1;
+            Quaternion rot = target.ExGetBase().transform.rotation;
+            ObjectPoolingManager.Instance.PopForDuration(gameObject, pos, rot, null, 3);
+        }
     }
 }
