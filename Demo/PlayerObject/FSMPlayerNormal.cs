3d67dab2fea994a313f7d92f574b3d773a6327e6
sjlee <tjdwn0406@gmail.com>
2026-09-25 11:41:27 +0900

    2D 컨트롤러 추가 수정

--- Demo/PlayerObject/FSMPlayerNormal.cs
+++ Demo/PlayerObject/FSMPlayerNormal.cs
@@ -1,300 +1,38 @@
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-
 namespace PahlUnity.Demo
 {
     public class FSMPlayerNormal : FiniteStateBase
     {
-        [SerializeField] private float _JumpForce = 12f;
-        [SerializeField] private bool _EnableWallAttach = false;
-        [SerializeField] private float _WallSlideSpeed = 1.5f;
-        [SerializeField] private float _WallAttachMinFallSpeed = 0.5f;
-        [SerializeField] private float _WallAttachBlockAfterJump = 0.25f;
-        [SerializeField] private Vector2 _WallJumpForce = new Vector2(8f, 12f);
-        [SerializeField] private float _DropDownSpeed = 3f;
-        [SerializeField] private float _DropDownMinDuration = 0.35f;
-        [SerializeField] private float _DropDownMaxDuration = 0.8f;
-        [SerializeField] private Vector2 _GroundCheckSize = new Vector2(0.42f, 0.08f);
-        [SerializeField] private Vector2 _WallCheckSize = new Vector2(0.08f, 1.3f);
-        [SerializeField] private float _CheckDistance = 0.04f;
-
         BaseObject mBase;
         PlayerController2D mPlayerCtrl;
-        FSMPlayerDash mDash;
-        FSMPlayerDeath mDeath;
-
-        readonly Collider2D[] mOverlapResults = new Collider2D[4];
-
-        int mTerrainLayerMask = 0;
-        int mThinPlatformLayerMask = 0;
-        int mGroundLayerMask = 0;
-        ContactFilter2D mTerrainContactFilter;
-        ContactFilter2D mThinPlatformContactFilter;
-        ContactFilter2D mGroundContactFilter;
-        bool mIsGrounded = false;
-        bool mIsWallAttached = false;
-        bool mDidDoubleJump = false;
-        bool mIsDroppingDown = false;
-        float mWallAttachBlockTimer = 0f;
-        int mDropDownMotionID = 0;
-
-        public bool IsDroppingDown => mIsDroppingDown;
 
         void Awake()
         {
             mBase = this.ExGetBase();
             mPlayerCtrl = mBase.GetComp<PlayerController2D>();
-            mDash = mBase.GetComp<FSMPlayerDash>();
-            mDeath = mBase.GetComp<FSMPlayerDeath>();
-
-            mTerrainLayerMask = GetLayerMask(LayerID.Terrain);
-            mThinPlatformLayerMask = GetLayerMask(LayerID.ThinPlatform);
-            mGroundLayerMask = mTerrainLayerMask | mThinPlatformLayerMask;
-            mTerrainContactFilter = CreateContactFilter(mTerrainLayerMask);
-            mThinPlatformContactFilter = CreateContactFilter(mThinPlatformLayerMask);
-            mGroundContactFilter = CreateContactFilter(mGroundLayerMask);
         }
 
         public override void UpdateState()
         {
             base.UpdateState();
 
-            UpdateBlockTimers();
-            UpdateContactState();
-            UpdateAnimatorParams();
+            if (!mPlayerCtrl.EnableWallAttach)
+                mPlayerCtrl.SetWallAttached(false);
 
-            if (!_EnableWallAttach)
-                SetWallAttached(false);
-
-            HandleDropDown();
+            mPlayerCtrl.HandleDropDown();
 
             if (mPlayerCtrl.HandleDash())
                 return;
 
-            HandleJump();
-            if (_EnableWallAttach)
-                HandleWallAttach();
-            HandleMove();
+            mPlayerCtrl.HandleJump();
+            if (mPlayerCtrl.EnableWallAttach)
+                mPlayerCtrl.HandleWallAttach();
+            mPlayerCtrl.DoMoveOnInput();
         }
 
         public override void LeaveState()
         {
             base.LeaveState();
-            SetWallAttached(false);
-        }
-
-        public void SetWallAttached(bool isAttached)
-        {
-            if (mIsWallAttached == isAttached)
-                return;
-
-            mIsWallAttached = isAttached;
-            mBase.Physics2D.LockGravity = isAttached;
-        }
-
-        public void InterruptDropDown()
-        {
-            ++mDropDownMotionID;
-            mBase.Body2D.LockThinPlatform = false;
-            mIsDroppingDown = false;
-        }
-
-        void UpdateBlockTimers()
-        {
-            if (mWallAttachBlockTimer > 0f)
-                mWallAttachBlockTimer -= Time.deltaTime;
-        }
-
-        void UpdateContactState()
-        {
-            mIsGrounded = mIsDroppingDown ? CheckTerrainGrounded() : CheckGrounded();
-            if (mIsGrounded)
-                mDidDoubleJump = false;
-        }
-
-        void HandleMove()
-        {
-            if (mIsWallAttached)
-                return;
-
-            float moveX = GetMoveX();
-            if (Mathf.Abs(moveX) > 0f)
-            {
-                mBase.Body2D.Turn(moveX);
-                mBase.Physics2D.VelocityX = moveX * mBase.Spec[SpecFields.MoveSpeed];
-            }
-            else
-            {
-                mBase.Physics2D.VelocityX = 0f;
-            }
-        }
-
-        void HandleJump()
-        {
-            if (!mBase.Input.JustPressed(InputActionNameHash.Jump) || mBase.Input.MoveY < -0.5f)
-                return;
-
-            if (_EnableWallAttach && mIsWallAttached)
-            {
-                DoWallJump();
-                return;
-            }
-
-            if (mIsGrounded)
-            {
-                mDidDoubleJump = false;
-                mWallAttachBlockTimer = _WallAttachBlockAfterJump;
-                mBase.Physics2D.DoJump(_JumpForce);
-                mBase.Anim.PlayAnim(AnimStateNameHash.Jump);
-            }
-            else if (!mDidDoubleJump)
-            {
-                mDidDoubleJump = true;
-                mWallAttachBlockTimer = _WallAttachBlockAfterJump;
-                mBase.Physics2D.DoJump(_JumpForce);
-                mBase.Anim.PlayAnim(AnimStateNameHash.Jump);
-            }
-        }
-
-        void HandleWallAttach()
-        {
-            bool canAttachWall = !mIsGrounded
-                              && mBase.Physics2D.VelocityY <= -_WallAttachMinFallSpeed
-                              && mWallAttachBlockTimer <= 0f
-                              && IsPressingToWall()
-                              && CheckWall();
-
-            if (!canAttachWall)
-            {
-                SetWallAttached(false);
-                return;
-            }
-
-            SetWallAttached(true);
-            mDidDoubleJump = false;
-            mBase.Physics2D.VelocityX = 0f;
-            mBase.Physics2D.VelocityY = -_WallSlideSpeed;
-        }
-
-        void DoWallJump()
-        {
-            int wallDir = mBase.Body2D.FrontDirInt;
-            int jumpDir = -wallDir;
-
-            SetWallAttached(false);
-            mWallAttachBlockTimer = _WallAttachBlockAfterJump;
-            mDidDoubleJump = false;
-            mBase.Body2D.Turn(jumpDir);
-            mBase.Physics2D.Velocity = new Vector2(jumpDir * _WallJumpForce.x, _WallJumpForce.y);
-            mBase.Anim.PlayAnim(AnimStateNameHash.Jump);
-        }
-
-        void HandleDropDown()
-        {
-            if (!mBase.Input.JustPressed(InputActionNameHash.Jump) || mBase.Input.MoveY >= -0.5f)
-                return;
-
-            RunDropDownAsync().Forget();
-        }
-
-        async UniTaskVoid RunDropDownAsync()
-        {
-            if (mIsDroppingDown || mBase.FSM.CurrentState != this)
-                return;
-
-            if (!mIsGrounded || !CheckThinPlatformGrounded())
-                return;
-
-            int motionID = ++mDropDownMotionID;
-            mIsDroppingDown = true;
-            mDidDoubleJump = false;
-            mBase.Body2D.LockThinPlatform = true;
-            mBase.Physics2D.VelocityY = -_DropDownSpeed;
-
-            float elapsedTime = 0f;
-            while (motionID == mDropDownMotionID
-                && mBase.FSM.CurrentState != mDeath
-                && elapsedTime < _DropDownMaxDuration
-                && (elapsedTime < _DropDownMinDuration || CheckThinPlatformGrounded()))
-            {
-                elapsedTime += Time.deltaTime;
-                await UniTask.Yield();
-            }
-
-            if (motionID == mDropDownMotionID)
-            {
-                mBase.Body2D.LockThinPlatform = false;
-                mIsDroppingDown = false;
-            }
-        }
-
-        void UpdateAnimatorParams()
-        {
-            float moveX = GetMoveX();
-            bool isMoving = Mathf.Abs(moveX) > 0f
-                         && mIsGrounded
-                         && !mIsWallAttached
-                         && mBase.FSM.CurrentState == this;
-
-            mBase.Anim.SetParamFloat(AnimatorParams.MoveSpeed, Mathf.Abs(moveX));
-            mBase.Anim.SetParamBool(AnimatorParams.IsGrounded, mIsGrounded);
-            mBase.Anim.SetParamBool(AnimatorParams.IsMoving, isMoving);
-            mBase.Anim.SetParamBool(AnimatorParams.IsWallAttached, mIsWallAttached);
-        }
-
-        bool CheckGrounded()
-        {
-            Vector2 checkCenter = mBase.Body2D.Foot.ExToVector2() + Vector2.down * _CheckDistance;
-            return Physics2D.OverlapBox(checkCenter, _GroundCheckSize, 0f, mGroundContactFilter, mOverlapResults) > 0;
-        }
-
-        bool CheckTerrainGrounded()
-        {
-            Vector2 checkCenter = mBase.Body2D.Foot.ExToVector2() + Vector2.down * _CheckDistance;
-            return Physics2D.OverlapBox(checkCenter, _GroundCheckSize, 0f, mTerrainContactFilter, mOverlapResults) > 0;
-        }
-
-        bool CheckThinPlatformGrounded()
-        {
-            Vector2 checkCenter = mBase.Body2D.Foot.ExToVector2() + Vector2.down * _CheckDistance;
-            return Physics2D.OverlapBox(checkCenter, _GroundCheckSize, 0f, mThinPlatformContactFilter, mOverlapResults) > 0;
-        }
-
-        bool CheckWall()
-        {
-            Vector2 checkCenter = mBase.Body2D.Center.ExToVector2() + Vector2.right * (mBase.Body2D.FrontDirInt * (mBase.Body2D.Size.x * 0.5f + _CheckDistance));
-            return Physics2D.OverlapBox(checkCenter, _WallCheckSize, 0f, mTerrainContactFilter, mOverlapResults) > 0;
-        }
-
-        bool IsPressingToWall()
-        {
-            float moveX = GetMoveX();
-            return Mathf.Abs(moveX) > 0f && Mathf.Sign(moveX) == mBase.Body2D.FrontDirInt;
-        }
-
-        float GetMoveX()
-        {
-            float moveX = mBase.Input.MoveX;
-            if (Mathf.Abs(moveX) < 0.01f)
-                return 0f;
-
-            return Mathf.Sign(moveX);
-        }
-
-        int GetLayerMask(int layerID)
-        {
-            if (layerID < 0)
-                return 0;
-
-            return 1 << layerID;
-        }
-
-        ContactFilter2D CreateContactFilter(int layerMask)
-        {
-            ContactFilter2D contactFilter = new ContactFilter2D();
-            contactFilter.useTriggers = false;
-            contactFilter.SetLayerMask(layerMask);
-            return contactFilter;
+            mPlayerCtrl.SetWallAttached(false);
         }
     }
 }
--- Demo/PlayerObject/PlayerController2D.cs
+++ Demo/PlayerObject/PlayerController2D.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace PahlUnity.Demo
@@ -16,10 +17,42 @@ namespace PahlUnity.Demo
     /// </summary>
     public class PlayerController2D : MonoBehaviour
     {
+        [SerializeField] private float _JumpForce = 12f;
+        [SerializeField] private bool _EnableWallAttach = false;
+        [SerializeField] private float _WallSlideSpeed = 1.5f;
+        [SerializeField] private float _WallAttachMinFallSpeed = 0.5f;
+        [SerializeField] private float _WallAttachBlockAfterJump = 0.25f;
+        [SerializeField] private Vector2 _WallJumpForce = new Vector2(8f, 12f);
+        [SerializeField] private float _DropDownSpeed = 3f;
+        [SerializeField] private float _DropDownMinDuration = 0.35f;
+        [SerializeField] private float _DropDownMaxDuration = 0.8f;
+        [SerializeField] private Vector2 _GroundCheckSize = new Vector2(0.42f, 0.08f);
+        [SerializeField] private Vector2 _WallCheckSize = new Vector2(0.08f, 1.3f);
+        [SerializeField] private float _CheckDistance = 0.04f;
 
         BaseObject mBaseObj = null;
         FiniteStateMachine mFSM = null;
         Dictionary<PlayerState, FiniteStateBase> mStates = new();
+
+        readonly Collider2D[] mOverlapResults = new Collider2D[4];
+
+        int mTerrainLayerMask = 0;
+        int mThinPlatformLayerMask = 0;
+        int mGroundLayerMask = 0;
+        ContactFilter2D mTerrainContactFilter;
+        ContactFilter2D mThinPlatformContactFilter;
+        ContactFilter2D mGroundContactFilter;
+        bool mIsGrounded = false;
+        bool mIsWallAttached = false;
+        bool mDidDoubleJump = false;
+        bool mIsDroppingDown = false;
+        float mWallAttachBlockTimer = 0f;
+        int mDropDownMotionID = 0;
+
+        public bool EnableWallAttach => _EnableWallAttach;
+        public bool IsGrounded => mIsGrounded;
+        public bool IsWallAttached => mIsWallAttached;
+        public bool IsDroppingDown => mIsDroppingDown;
 
         void Awake()
         {
@@ -43,7 +76,21 @@ namespace PahlUnity.Demo
                     PlayDamagedMotion();
             };
 
+            mTerrainLayerMask = GetLayerMask(LayerID.Terrain);
+            mThinPlatformLayerMask = GetLayerMask(LayerID.ThinPlatform);
+            mGroundLayerMask = mTerrainLayerMask | mThinPlatformLayerMask;
+            mTerrainContactFilter = CreateContactFilter(mTerrainLayerMask);
+            mThinPlatformContactFilter = CreateContactFilter(mThinPlatformLayerMask);
+            mGroundContactFilter = CreateContactFilter(mGroundLayerMask);
+
             BindStates();
+        }
+
+        void Update()
+        {
+            UpdateBlockTimers();
+            UpdateContactState();
+            UpdateAnimatorParams();
         }
 
         void Start()
@@ -107,10 +154,224 @@ namespace PahlUnity.Demo
                 return false;
 
             FSMPlayerDash dashState = mStates[PlayerState.Dashing] as FSMPlayerDash;
-            if (!dashState.IsDashable())
+            if (!dashState.IsDashable() || mIsDroppingDown)
                 return false;
 
             return ChangeState(PlayerState.Dashing);
+        }
+
+        public void DoMoveOnInput()
+        {
+            if (mIsWallAttached)
+                return;
+
+            float moveX = GetMoveX();
+            if (Mathf.Abs(moveX) > 0f)
+            {
+                mBaseObj.Body2D.Turn(moveX);
+                mBaseObj.Physics2D.VelocityX = moveX * mBaseObj.Spec[SpecFields.MoveSpeed];
+            }
+            else
+            {
+                mBaseObj.Physics2D.VelocityX = 0f;
+            }
+        }
+
+        public void HandleJump()
+        {
+            if (!mBaseObj.Input.JustPressed(InputActionNameHash.Jump) || mBaseObj.Input.MoveY < -0.5f)
+                return;
+
+            if (_EnableWallAttach && mIsWallAttached)
+            {
+                DoWallJump();
+                return;
+            }
+
+            if (mIsGrounded)
+            {
+                mDidDoubleJump = false;
+                mWallAttachBlockTimer = _WallAttachBlockAfterJump;
+                mBaseObj.Physics2D.DoJump(_JumpForce);
+                mBaseObj.Anim.PlayAnim(AnimStateNameHash.Jump);
+            }
+            else if (!mDidDoubleJump)
+            {
+                mDidDoubleJump = true;
+                mWallAttachBlockTimer = _WallAttachBlockAfterJump;
+                mBaseObj.Physics2D.DoJump(_JumpForce);
+                mBaseObj.Anim.PlayAnim(AnimStateNameHash.Jump);
+            }
+        }
+
+        public void HandleWallAttach()
+        {
+            bool canAttachWall = !mIsGrounded
+                              && mBaseObj.Physics2D.VelocityY <= -_WallAttachMinFallSpeed
+                              && mWallAttachBlockTimer <= 0f
+                              && IsPressingToWall()
+                              && CheckWall();
+
+            if (!canAttachWall)
+            {
+                SetWallAttached(false);
+                return;
+            }
+
+            SetWallAttached(true);
+            mDidDoubleJump = false;
+            mBaseObj.Physics2D.VelocityX = 0f;
+            mBaseObj.Physics2D.VelocityY = -_WallSlideSpeed;
+        }
+
+        public void HandleDropDown()
+        {
+            if (!mBaseObj.Input.JustPressed(InputActionNameHash.Jump) || mBaseObj.Input.MoveY >= -0.5f)
+                return;
+
+            RunDropDownAsync().Forget();
+        }
+
+        public void SetWallAttached(bool isAttached)
+        {
+            if (mIsWallAttached == isAttached)
+                return;
+
+            mIsWallAttached = isAttached;
+            mBaseObj.Physics2D.LockGravity = isAttached;
+        }
+
+        public void InterruptDropDown()
+        {
+            ++mDropDownMotionID;
+            mBaseObj.Body2D.LockThinPlatform = false;
+            mIsDroppingDown = false;
+        }
+
+        void DoWallJump()
+        {
+            int wallDir = mBaseObj.Body2D.FrontDirInt;
+            int jumpDir = -wallDir;
+
+            SetWallAttached(false);
+            mWallAttachBlockTimer = _WallAttachBlockAfterJump;
+            mDidDoubleJump = false;
+            mBaseObj.Body2D.Turn(jumpDir);
+            mBaseObj.Physics2D.Velocity = new Vector2(jumpDir * _WallJumpForce.x, _WallJumpForce.y);
+            mBaseObj.Anim.PlayAnim(AnimStateNameHash.Jump);
+        }
+
+        async UniTaskVoid RunDropDownAsync()
+        {
+            if (mIsDroppingDown || !IsCurrentState(PlayerState.Normal))
+                return;
+
+            if (!mIsGrounded || !CheckThinPlatformGrounded())
+                return;
+
+            int motionID = ++mDropDownMotionID;
+            mIsDroppingDown = true;
+            mDidDoubleJump = false;
+            mBaseObj.Body2D.LockThinPlatform = true;
+            mBaseObj.Physics2D.VelocityY = -_DropDownSpeed;
+
+            float elapsedTime = 0f;
+            while (motionID == mDropDownMotionID
+                && !IsCurrentState(PlayerState.Dead)
+                && elapsedTime < _DropDownMaxDuration
+                && (elapsedTime < _DropDownMinDuration || CheckThinPlatformGrounded()))
+            {
+                elapsedTime += Time.deltaTime;
+                await UniTask.Yield();
+            }
+
+            if (motionID == mDropDownMotionID)
+            {
+                mBaseObj.Body2D.LockThinPlatform = false;
+                mIsDroppingDown = false;
+            }
+        }
+
+        void UpdateBlockTimers()
+        {
+            if (mWallAttachBlockTimer > 0f)
+                mWallAttachBlockTimer -= Time.deltaTime;
+        }
+
+        void UpdateContactState()
+        {
+            mIsGrounded = mIsDroppingDown ? CheckTerrainGrounded() : CheckGrounded();
+            if (mIsGrounded)
+                mDidDoubleJump = false;
+        }
+
+        void UpdateAnimatorParams()
+        {
+            float moveX = GetMoveX();
+            bool isMoving = Mathf.Abs(moveX) > 0f
+                         && mIsGrounded
+                         && !mIsWallAttached
+                         && IsCurrentState(PlayerState.Normal);
+
+            mBaseObj.Anim.SetParamFloat(AnimatorParams.MoveSpeed, Mathf.Abs(moveX));
+            mBaseObj.Anim.SetParamBool(AnimatorParams.IsGrounded, mIsGrounded);
+            mBaseObj.Anim.SetParamBool(AnimatorParams.IsMoving, isMoving);
+            mBaseObj.Anim.SetParamBool(AnimatorParams.IsWallAttached, mIsWallAttached);
+        }
+
+        bool CheckGrounded()
+        {
+            Vector2 checkCenter = mBaseObj.Body2D.Foot.ExToVector2() + Vector2.down * _CheckDistance;
+            return Physics2D.OverlapBox(checkCenter, _GroundCheckSize, 0f, mGroundContactFilter, mOverlapResults) > 0;
+        }
+
+        bool CheckTerrainGrounded()
+        {
+            Vector2 checkCenter = mBaseObj.Body2D.Foot.ExToVector2() + Vector2.down * _CheckDistance;
+            return Physics2D.OverlapBox(checkCenter, _GroundCheckSize, 0f, mTerrainContactFilter, mOverlapResults) > 0;
+        }
+
+        bool CheckThinPlatformGrounded()
+        {
+            Vector2 checkCenter = mBaseObj.Body2D.Foot.ExToVector2() + Vector2.down * _CheckDistance;
+            return Physics2D.OverlapBox(checkCenter, _GroundCheckSize, 0f, mThinPlatformContactFilter, mOverlapResults) > 0;
+        }
+
+        bool CheckWall()
+        {
+            Vector2 checkCenter = mBaseObj.Body2D.Center.ExToVector2() + Vector2.right * (mBaseObj.Body2D.FrontDirInt * (mBaseObj.Body2D.Size.x * 0.5f + _CheckDistance));
+            return Physics2D.OverlapBox(checkCenter, _WallCheckSize, 0f, mTerrainContactFilter, mOverlapResults) > 0;
+        }
+
+        bool IsPressingToWall()
+        {
+            float moveX = GetMoveX();
+            return Mathf.Abs(moveX) > 0f && Mathf.Sign(moveX) == mBaseObj.Body2D.FrontDirInt;
+        }
+
+        float GetMoveX()
+        {
+            float moveX = mBaseObj.Input.MoveX;
+            if (Mathf.Abs(moveX) < 0.01f)
+                return 0f;
+
+            return Mathf.Sign(moveX);
+        }
+
+        int GetLayerMask(int layerID)
+        {
+            if (layerID < 0)
+                return 0;
+
+            return 1 << layerID;
+        }
+
+        ContactFilter2D CreateContactFilter(int layerMask)
+        {
+            ContactFilter2D contactFilter = new ContactFilter2D();
+            contactFilter.useTriggers = false;
+            contactFilter.SetLayerMask(layerMask);
+            return contactFilter;
         }
     }
 }
